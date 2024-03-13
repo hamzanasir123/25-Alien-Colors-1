64bac42b2a24c745e42e8a779decf190d5c35675
Hamza Nasir <161011944+hamzanasir123@users.noreply.github.com>
2024-03-13 14:37:37 +0500

    first commit

---
+++ Alien_Colors1.ts
@@ -0,0 +1,54 @@
+// Alien Colors #1: Imagine an alien was just shot down in a game.
+//  Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+
+// Create a variable called alien_color 
+let alien_colour :string = 'Green';
+
+// Write an if statement to test whether the alien’s color is green.
+//  If it is, print a message that the player just earned 5 points.
+if(alien_colour === 'Green'){
+    console.log("Player just earned 5 points !");
+}
+
+// Write one version of this program that passes the if test and another that fails.
+//  (The version that fails will have no output.)
+
+alien_colour = 'Yellow';
+
+if(alien_colour === 'Green'){
+    console.log("Player just earned 5 points !");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
